954a4585a2eb06c7426a9093186a81bcef46d3e7
itc-21011 <s21011@std.it-college.ac.jp>
2022-07-01 22:01:41 +0900

    課題

---
+++ src/main/kotlin/jp/ac/it_college/std/s21011/news_manager/infrastructure/database/record/Category.kt
@@ -0,0 +1,9 @@
+/*
+ * Auto-generated file. Created by MyBatis Generator
+ */
+package jp.ac.it_college.std.s21011.news_manager.infrastructure.database.record
+
+data class Category(
+    var id: Long? = null,
+    var name: String? = null
+)
